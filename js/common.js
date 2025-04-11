ee6df04910c93ba32a60ae1f14216dc33e52da18
Gadzhi Gadzhiev <47302253+Callisto7@users.noreply.github.com>
2025-04-11 18:08:57 +0300

    add videos

--- js/common.js
+++ js/common.js
@@ -20,29 +20,3 @@ setInterval(() => {
     rotatingText.classList.remove("hidden");
   }, 500);
 }, 3000);
-
-// document.querySelectorAll(".button-play").forEach((button) => {
-//   button.addEventListener("click", () => {
-//     const container = button.closest(
-//       ".value-prop__image, .portfolio-item__image"
-//     );
-//     const img = container.querySelector("img");
-//     const video = container.querySelector("video");
-
-//     button.style.display = "none";
-//     img.style.display = "none";
-//     video.style.display = "block";
-//     video.play();
-
-//     // Автоматическое возвращение в начальное состояние после окончания
-//     video.addEventListener(
-//       "ended",
-//       () => {
-//         video.style.display = "none";
-//         img.style.display = "block";
-//         button.style.display = "block";
-//       },
-//       { once: true }
-//     );
-//   });
-// });
