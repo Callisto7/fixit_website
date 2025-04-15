1c4acc92ecf5ad21ae0a796ed1edb46db7c81642
Gadzhi Gadzhiev <47302253+Callisto7@users.noreply.github.com>
2025-04-15 09:50:25 +0300

    fix scrolling mouse

--- js/common.js
+++ js/common.js
@@ -57,3 +57,34 @@ function closeModal() {
   modalVideo.src = ""; // Сброс — останавливает видео
 }
 modal.addEventListener("click", closeModal);
+
+const slider = document.getElementById('portfolioSlider');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('grabbing');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('grabbing');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('grabbing');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1.5; // можно настроить чувствительность
+  slider.scrollLeft = scrollLeft - walk;
+});
