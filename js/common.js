53d7315953d49d32c3d16e174450896e0ed64d45
Gadzhi Gadzhiev <47302253+Callisto7@users.noreply.github.com>
2025-04-18 09:35:08 +0300

    returned by scrolling-press

--- js/common.js
+++ js/common.js
@@ -57,3 +57,33 @@ function closeModal() {
   modalVideo.src = ""; // Сброс — останавливает видео
 }
 modal.addEventListener("click", closeModal);
+
+const slider = document.getElementById("portfolioSlider");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active"); // можно использовать для визуального эффекта
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1.5; // чувствительность скролла
+  slider.scrollLeft = scrollLeft - walk;
+});
